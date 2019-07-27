a439d4beed5c567e3c5ae2e2e375ae03a2d3e5db
Roanak Baviskar <roanakbaviskar@gmail.com>
2019-07-26 22:30:33 -0700

    modularized

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
