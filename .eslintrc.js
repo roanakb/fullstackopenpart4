d1f472896155d9badb812e1821a69b3f4f486616
Roanak Baviskar <roanakbaviskar@gmail.com>
2019-07-29 17:44:54 -0700

    finished 4.3

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es6': true,
+		"jest": true,
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018
+	},
+	'rules': {
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
