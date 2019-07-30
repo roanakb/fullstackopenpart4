c222b896dbc7e1d25a8e2a250340ca43f97d3c6b
Roanak Baviskar <roanakbaviskar@gmail.com>
2019-07-29 18:05:22 -0700

    finished 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,9 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => { return blogs.reduce((sum, item) => {return sum + item.likes}, 0) }
+
 module.exports = {
-	dummy
+    dummy,
+    totalLikes,
 }
