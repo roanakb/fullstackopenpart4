46d65709f27fab0d926cb6a61ed3316664129182
Roanak Baviskar <roanakbaviskar@gmail.com>
2019-07-29 18:20:29 -0700

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,7 +4,24 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => { return blogs.reduce((sum, item) => {return sum + item.likes}, 0) }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    }
+    else {
+        return blogs.reduce((fav, item) => {
+            if (item.likes >= fav.likes) {
+                return item
+            }
+            else {
+                return fav
+            }
+        }, { likes: 0 })
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 }
